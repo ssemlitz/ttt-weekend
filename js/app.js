591bb79910ff9039f095a35224fc9d54174611c3
ssemlitz <sdsemlitz@yahoo.com>
2022-06-01 12:10:55 -0400

    initialize cashed element references

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const squareEls = document.querySelectorAll('.squares')
+const messageEl = document.querySelector('#message')
+ console.log(squareEls)
+ console.log(messageEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -16,3 +18,6 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+function init() {
+  
+}
