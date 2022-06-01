9a01bfbf160c91097d7b757fd54f5d507e2fac22
ssemlitz <sdsemlitz@yahoo.com>
2022-06-01 12:40:10 -0400

    functioning init and render function

--- js/app.js
+++ js/app.js
@@ -17,7 +17,28 @@ const messageEl = document.querySelector('#message')
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
 function init() {
-  
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = null
+  render()
 }
+
+function render() {
+  board.forEach((square, index) => {
+    if (square === -1) {
+      squareEls[index].textContent = 'O'
+    } else if (square === 1) {
+      squareEls[index].textContent = 'X'
+    } else {
+      squareEls[index].textContent = null
+    }
+  });
+
+//   if (winner === null) {
+//     if (turn === 1) {
+//       messageEl.textContent = 
+//   } 
+// }
