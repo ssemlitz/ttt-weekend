9c4e3682ae01b6f88fac5012a6c3a3a4b2af396e
ssemlitz <sdsemlitz@yahoo.com>
2022-06-01 16:33:44 -0400

    working getWinner function

--- js/app.js
+++ js/app.js
@@ -39,13 +39,13 @@ function render() {
     }
   });
 
-  if (winner === null) {
-    return (turn === 1 ? messageEl.textContent = "Player 1's turn!" : messageEl.textContent = "Player 2's turn!")
-  } else if (winner === 'T') {
-    return messageEl.textContent = 'We have a tie!'
-  } else {
-    return (winner === 1 ? messageEl.textContent = "Player 1 has won it!" : messageEl.textContent = "Player 2 has won it!") 
-  }
+    if (winner === null) {
+      return (turn === 1 ? messageEl.textContent = "Player 1's turn!" : messageEl.textContent = "Player 2's turn!")
+    } else if (winner === 'T') {
+      return messageEl.textContent = 'We have a tie!'
+    } else {
+      return (winner === 1 ? messageEl.textContent = "Player 1 has won it!" : messageEl.textContent = "Player 2 has won it!") 
+    }
 }
 
 function handleClick(evt) {
@@ -53,24 +53,33 @@ function handleClick(evt) {
 
 
   if (board[sqIdx] !== null || winner !== null) {
-    console.log(sqIdx)
-    console.log(winner)
     return
   } else {
     board[sqIdx] = turn
-    console.log(board)
     turn *= -1
-    console.log(turn)
   }
+  winner = getWinner()
   render()
 }
 
 function getWinner() {
+  let foundWinner = null
+  
   winningCombos.forEach(combo => {
-    if (Maths.abs(board[combo[0]] + board[combo[1]] + board[combo[2]] === 3)) {
-      winner = turn
-    } else {
-      winner = 'T'
+    if (Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]]) === 3) {
+      foundWinner = board[combo[0]]
     }
   })
+  console.log(foundWinner)
+  if (foundWinner) return foundWinner
+  if (!board.includes(null)){
+    return 'T'
+  } else {
+    return null
+  }
+}
+
+// function reset() {
+//   init()
+
 }
